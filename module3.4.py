135beb4e43fc5b6e33bbcaf86943c41c6db1d78b
9494Guzel <guzel.galiulina.94@inbox.ru>
2024-07-30 19:28:07 +0300

    Add files via upload

---
+++ module3.4.py
@@ -0,0 +1,8 @@
+def single_root_words(n= 'root_word', **other_words):
+    print(n, b='rich', a='aple')
+    print(single_root_words)
+
+
+for i in single_root_words():
+    print(i)
+    print(n, rich, aple)
